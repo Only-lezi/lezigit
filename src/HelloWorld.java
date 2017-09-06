57e79f7c31fd05b5ba196d39e4a1c8574063c206
Only-lezi <799795136@qq.com>
2017-09-07 00:46:49 +0800

    Revert "开发提交"
    This reverts commit 8e37ac9

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("代码提交开发");
+		// TODO Auto-generated method stub
 		System.out.println("HelloWorld！");
 	}
 
